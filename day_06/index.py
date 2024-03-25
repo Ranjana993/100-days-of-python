ed9de4599db9bde385e8bc85f301f577f7adc616
Kumari Ranjana Yadav <yranjana757@gmail.com>
2024-03-25 21:41:32 +0530

    day 7 is done

--- day_06/index.py
+++ day_06/index.py
@@ -1,5 +1,5 @@
 
 # taking input from user 
-first_number= int(input("Enter your first number ")) 
-second_number = int(input("Enter your second number ") )
+first_number= input("Enter your first number ") 
+second_number = input("Enter your second number ") 
 print(first_number + second_number)
