fd8c54cf7ef1b5a65e4d544cfef67569c0369521
Kumari Ranjana Yadav <yranjana757@gmail.com>
2024-03-25 21:19:02 +0530

    day 5 is done

---
+++ day_05/index.py
@@ -0,0 +1,31 @@
+
+# ! Keywords =>  in programming a keyword is a word that is reserved by a  program because the word has a special meaning . Keywords can be commands or parameters . Every programming language has a set of keywords that cannot be used as variable names .
+
+# ['False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
+
+import keyword
+# print(keyword.kwlist)
+
+
+
+
+# ! Identifiers : A python is a name used to identify a variale , function , class , module or other object
+
+#  Rules for setting identifiers 
+
+# -> can only start with an alphabet or _ 
+_ = "ranjana" 
+print(_)
+
+# -> followed by 0 or more letter and digits
+first_name = "ranjana"
+print(first_name)
+
+# -> keywords cannot be used as an identifier
+
+# if = "ranjana"
+# print(if)
+
+
+
+
