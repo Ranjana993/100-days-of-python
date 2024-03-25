8eb32d7c79bce04f6b8df1c0c17b4262f5cb578e
Kumari Ranjana Yadav <yranjana757@gmail.com>
2024-03-25 22:02:02 +0530

    day 8 is done

---
+++ day_08/index.py
@@ -0,0 +1,36 @@
+
+# ! Literals => literals are a raw given in a variable . In python there are variable types of literals they are as follows:
+#  Numeric literals
+# String literals
+# Boolean literals
+# Special literals
+
+# Numeric literals => ( int , float and complex literals ) who deals with numerical values 
+
+a = 12
+b = 23.4
+c = 4+ 45j
+
+
+#  String literals => 
+
+string = 'ranjana'
+string = "ranjana"
+char = "C"
+multiline_str = """ iterals are a raw given in a variable . In python there are variable types of literals they are as follows: """
+raw_str = r"raw \n string"
+smile = u"\U0001f600"
+# print(smile)
+
+
+
+#  Boolean literals => 
+a = True + 4 
+b = False + 10
+# print ("a" , a )
+# print("b" , b)
+
+
+# Special literals =>
+a = None
+print(a) 
