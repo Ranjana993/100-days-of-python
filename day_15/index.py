8195a874f2896e142aaef631243b1c64f8fc1ff0
Kumari Ranjana Yadav <yranjana757@gmail.com>
2024-03-26 12:50:14 +0530

    day 15 done

---
+++ day_15/index.py
@@ -0,0 +1,28 @@
+
+#! nested loop
+
+
+#  *
+#  * *
+#  * * *
+#  * * * *
+#  * * * * * 
+
+row = 5
+
+for i in range (0 , row+1):
+  for j in range(0, i):
+    print(" * " , end=" ")
+  print("")
+
+
+
+
+
+
+
+
+
+
+
+
