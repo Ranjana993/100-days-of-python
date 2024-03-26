2d9ab361ad5748a4731aaaa10d86e0bb1a7ac06a
Kumari Ranjana Yadav <yranjana757@gmail.com>
2024-03-26 12:06:26 +0530

    day 11 done

---
+++ day_11/index.py
@@ -0,0 +1,19 @@
+
+# ! indention 
+
+# if we talk about other programming language then it doesn't matter if you are giving any indention or not but in python it is must because in python there is not concept of brackets 
+
+# name = "123"
+# if(name == "12344"){
+# print("correct")
+# }else{
+#  print("wrong")
+# }
+
+# ! but in python 
+
+name = "123"
+if name == "12344":
+  print("correct")
+else:
+  print("incorrect")  
