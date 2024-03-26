d9c3c6ea2b965d987335795fc9b98e40cd62e756
Kumari Ranjana Yadav <yranjana757@gmail.com>
2024-03-26 13:16:08 +0530

    day 17 done

---
+++ day_17/index.py
@@ -0,0 +1,49 @@
+
+# ! built in  function 
+
+#  print , input , type , int etc ,  abs , pow , min/max, round , divmod , id , bin/oct/hex , ord , len ,sum , help ,  
+
+# print
+print("abs")
+
+# input 
+input("Enter your input ")
+
+#  type 
+a = 23 
+type( a )
+
+
+# int 
+int(input("enter your age "))
+
+# abs 
+abs(-2)
+# pow
+pow(2,3)
+# min
+min([1,2,3,4,4,4,2,1])
+
+max([ 1,2,3,4,])
+
+# round
+round(2.4)
+
+a = 12
+print(id(a))
+
+# divmod 
+divmod(234)
+
+# bin/oct/hex
+bin(2)
+
+# len
+len([1,2,3,4])
+
+# sum
+sum([1,2,3,4])
+
+# help
+help()
+
