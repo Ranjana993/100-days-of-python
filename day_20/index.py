bfb6912e2a9f3d33d5d01d447402dafc5ce1d031
Kumari Ranjana Yadav <yranjana757@gmail.com>
2024-03-26 13:55:06 +0530

    day 20 done

---
+++ day_20/index.py
@@ -0,0 +1,28 @@
+
+# ! Accessing substring from a string
+
+a = "hello"
+# print(a)
+
+#! Type of indexing
+# positive indexing
+# print(a[2])
+
+# negative indexing
+# print(a[-4])
+
+# ! Slicing 
+b = "Hello World "
+# print(b[0 : 4])
+# print(b[2:])
+# print(b[:5])
+print(b[0:8:1])
+print(b[::-1])
+
+
+
+
+
+
+
+
