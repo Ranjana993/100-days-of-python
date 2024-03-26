8419f4fec612510955eb3b5d5d268d85c59b09f9
Kumari Ranjana Yadav <yranjana757@gmail.com>
2024-03-26 12:42:23 +0530

    day 14 done

---
+++ day_14/index.py
@@ -0,0 +1,24 @@
+
+# ! for loop
+# res = list(range(1 , 12))
+# print(res)
+
+# res1 = list(range(1, 12 , 2))
+# print(res1)
+
+
+
+# ! sequence :: python travesre in sequence manner
+# string 
+name = "ranjana"
+list1 = [ "ranjana" , "yadav" , "tester" ]
+
+
+# for i in range(1 , 12):
+  # print(i)
+
+# for i in "kolkata":
+  # print(i)  
+
+# for i in [1,2,3,4,5,6,7,8]:
+  # print(i)  
