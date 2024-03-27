c71dee3e84be64636a53017a918c76ea33941a77
Kumari Ranjana Yadav <yranjana757@gmail.com>
2024-03-27 12:47:29 +0530

    day  23 done

---
+++ day_23/index.py
@@ -0,0 +1,77 @@
+
+#! String function 
+
+# Common Functions
+
+a = "kolkata"
+
+# 1. len
+
+# print(len(a))
+
+#! 2. max
+# print(max(a))
+
+# !3. min
+# print(min(a))
+
+# !4. sorted
+# print(sorted(a))
+# print(sorted(a , reverse=True))
+
+
+# ! Capitalize / Title / Upper / Lower /Swapcase
+# a = "ranjana"
+# print(a.capitalize())
+# print(a.title())
+# print(a.upper())
+# print(a.lower())
+# print(a.swapcase())
+
+# ! count
+
+# print("it is raning ".count("i"))
+
+# !find / index
+# print("it is raning".find("g"))
+# print("it is raning".find("p"))
+# print("it is raning".index("r"))
+
+# ! endswith/startswith
+# print("it is raining".endswith("ing"))
+# print("it is raining".endswith("is"))
+
+# ! format
+
+# print("Hello my name is {} and i am {} year old ".format("ranjana" , 21))
+# print("Hello my name is {1} and i am {0} year old ".format("ranjana" , 21))
+
+
+# ! isalnum/isalpha/isdecimal/isdigit/isidentifier
+# print('flat23'.isalnum())
+# print("flat".isalpha())
+# print("20A".isdigit())
+# print("flat".isidentifier())
+# print("roll".isidentifier())
+
+
+# ! split
+# print("my name is ranjana".split())
+
+# ! join
+# print("/".join(['my', 'name', 'is', 'ranjana']))
+
+# ! replace
+# print("Hello my name is ranjan".replace("ranjan", "Ranjana"))
+
+# ! Strip
+name ="               ranjana "    
+print(name)            
+print(name.strip())
+
+
+
+
+
+
+
