8a0328b4fa695095649d991d8bd519d9fcbc4b9c
Kumari Ranjana Yadav <yranjana757@gmail.com>
2024-03-28 20:25:37 +0530

    day 25 done

---
+++ day_25/index.py
@@ -0,0 +1,73 @@
+
+#  Tuples 
+
+
+
+
+
+
+
+# Sets
+
+# Dictioonary
+
+# Problems
+
+# ! Tuples
+#  create 
+
+# !!
+T1 = ()
+T2 = (1,2,3,4,5,6)
+# print(T2)
+T3 = ("Hello" , 1,2,3 , True)
+# print(T3)
+T4 =(1,2,3,4 , (2,3,4));
+# print(T4)
+
+t5 = ("hello")
+# print(type(t5))
+t6 = ("hello" ,)
+# print(type(t6))
+t6 = tuple([1,2,3,4,5])
+# print(t6)
+# print(type(t6))
+
+#! Access 
+# print(T2[0])
+# print(T2[0:4])
+# print(T2[3:])
+# print(T2[:5])
+
+
+
+# ! Edit => we can not edit tupe's item coz it is immutable 
+# t1 = (1,2,3,4,5,6,7,8)
+# t1[2]=300
+# print(t1)
+
+
+#! Add  => we can not add items to tuples 
+
+#! Delete =>  we can delete whole tuples but we can not delete any part of it .
+# print(t1)
+# del t1
+
+# print(t1)
+
+# ! Operations
+t1 = (1,2,3,4,5,6,7,8)
+t2 = (9,8,7,6,5)
+# t3 = t1+ t2 
+# print(t3)
+
+# for i in t1 :
+#   print(i)
+
+# print( 2 in t1)
+# print(sum(t1))
+# print(max(t1))
+# print(min(t1))
+# print(sorted(t1 , reverse=True) )
+
+
